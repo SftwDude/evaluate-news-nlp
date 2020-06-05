786830dc37b55572cea7d645295c75c846fb3b71
Mike Berghoff <mberghoff@amt-corp.com>
2020-06-05 08:38:17 -0400

    Code upto lesson 4

--- webpack.dev.js
+++ webpack.dev.js
@@ -7,13 +7,21 @@ module.exports = {
     entry: './src/client/index.js',
     mode: 'development',
     devtool: 'source-map',
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
     stats: 'verbose',
     module: {
         rules: [
             {
-                test: '/\.js$/',
+                test: /\.js$/,
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
             }
         ]
     },
