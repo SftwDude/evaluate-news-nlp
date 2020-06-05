3bcfa3612db181aaac26225d1a1fc88e4cdc1d03
Mike Berghoff <mberghoff@amt-corp.com>
2020-06-05 15:54:18 -0400

    Add url validation

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -2,15 +2,15 @@ function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    Client.checkForName(formText)
+    let formText = document.getElementById('url').value
+    //Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
-        .then(res => res.json())
-        .then(function (res) {
-            document.getElementById('results').innerHTML = res.message
-        })
+    //fetch('http://localhost:8081/test')
+    //    .then(res => res.json())
+    //    .then(function (res) {
+    //        document.getElementById('results').innerHTML = res.message
+    //    })
 
     if (Client.validURL(formText)) {
         fetch(`http://localhost:8081/sentiment?url=${formText}`)
